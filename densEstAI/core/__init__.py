666719ee95b884c6150321065809e44faaf61e68
joon0890 <kimjunhee2483@gmail.com>
2025-08-27 14:58:30 +0900

    삭제

--- densEstAI/core/__init__.py
+++
@@ -1,3 +0,0 @@
-from .stream.video_streaming import ThreadedVideoStreamer
-from .yolo.preprocessor import PreProcessor 
-from .yolo.yolo_manager import YoloManager 
--- densEstAI/core/utils/html_handler.py
+++
@@ -1,6 +0,0 @@
-def save_html(output_path, template, progress_logs, density_data):
-    """HTML 파일 생성 및 저장"""
-    new_html = template.format(Progress_Logs=progress_logs, Density_Data=density_data)
-    with open(output_path, "w", encoding="utf-8") as file:
-        file.write(new_html)
-    print(f"HTML updated and saved to {output_path}")
